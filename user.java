47eef720e68592819331353721ea500deb7b0e22
MeghanaSurasani <142423176+MeghanaSurasani@users.noreply.github.com>
2023-11-21 15:10:35 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,35 @@
+package com.tns.controller;
+
+public class User {
+	long id;
+	String name;
+	String type;
+	String password;
+	public User() {
+		
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType() {
+		this.type = type;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
